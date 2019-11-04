baf2929665b96031a84105106fadb6e9412d1533
Philip Jägenstedt <philip@foolip.org>
2019-11-04 21:11:35 +0100

    Try iOS simulator

--- screenshot.py
+++ screenshot.py
@@ -2,17 +2,12 @@
 
 from selenium import webdriver
 
-def set_content_size(driver, width, height):
-    # This assumes that the size of the chrome doesn't change when the window
-    # size changes, which isn't necessarily true, but seems to be true.
-    window_size = driver.get_window_size()
-    content_size = driver.execute_script('return { width: innerWidth, height: innerHeight }')
-    chrome_width = window_size['width'] - content_size['width']
-    chrome_height = window_size[u'height'] - content_size[u'height']
-    driver.set_window_size(width + chrome_width, height + chrome_height)
-
-driver = webdriver.Safari(executable_path='/Applications/Safari Technology Preview.app/Contents/MacOS/safaridriver')
-set_content_size(driver, 800, 600)
+capabilities = {
+    "platformName": "iOS",
+    "safari:useSimulator": True,
+    "safari:deviceType": "iPad",
+}
+driver = webdriver.Safari(desired_capabilities=capabilities)
 driver.get('https://www.apple.com/safari/')
 driver.get_screenshot_as_file('screenshot.png')
 driver.quit()
